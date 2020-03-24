5b06200c9632147a42f4152dc5373545c762bfea
Neroli-realy <alyyaser12@gmail.com>
2020-03-24 19:52:48 +0200

    Update APIkeys.js

--- constants/APIkeys.js
+++ constants/APIkeys.js
@@ -1,11 +1,11 @@
 export default {
 	FirebaseConfig: {
-		apiKey: "AIzaSyCcqIHPhQIG79SqrTHf9PkaqaAmdh0rD1M",
-    	authDomain: "todo-42c6a.firebaseapp.com",
-    	databaseURL: "https://todo-42c6a.firebaseio.com",
-    	projectId: "todo-42c6a",
-    	storageBucket: "todo-42c6a.appspot.com",
-    	messagingSenderId: "983309451864",
-    	appId: "1:983309451864:web:e2eb0f72264d3b1dad802d"
+		apiKey: "",
+    	authDomain: "",
+    	databaseURL: "",
+    	projectId: "",
+    	storageBucket: "",
+    	messagingSenderId: "",
+    	appId: ""
 	}
 }
